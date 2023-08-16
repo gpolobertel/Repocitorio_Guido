38f0ad6d8f0d57f09da2544d1145d94883514a2d
gpolobertel <gjpolob@academia.usbbog.edu.co>
2023-08-16 12:40:03 -0500

    main

---
+++ sesion_1/Guido_Polo_taller#2/Mediciones.py
@@ -0,0 +1,3 @@
+pulgadas = float(input("Ingresa una medida en pulgadas: "))
+milimetros = pulgadas * 25.4
+print(pulgadas, "pulgadas equivale a", milimetros, "milímetros.")
---
+++ sesion_1/Guido_Polo_taller#2/iva.py
@@ -0,0 +1,3 @@
+precio_producto = float(input("Ingresa el precio del producto: "))
+iva = precio_producto * 0.19
+print("El valor del IVA es:", iva)
